b0a919475c3676515e136ec766cfdaa97d66808d
Jorge Correa <38728415+jarccorrea@users.noreply.github.com>
2025-06-11 20:02:18 -0300

    Update perguntas_respostas.py

--- perguntas_respostas.py
+++ perguntas_respostas.py
@@ -10,95 +10,44 @@ perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
         'Opções': ['1', '3', '4', '5'],
-        'Resposta': '4',
+        'Resposta': ('4', 'c', 'C')
     },
     {
         'Pergunta': 'Quanto é 5*5?',
         'Opções': ['25', '55', '10', '51'],
-        'Resposta': '25',
+        'Resposta': ('25', 'a', 'A')
     },
     {
         'Pergunta': 'Quanto é 10/2?',
         'Opções': ['4', '5', '2', '1'],
-        'Resposta': '5',
+        'Resposta': ('5', 'c', 'C')
     },
 ]
 
-# Função para a primeira pergunta
-def primeira_pergunta():
-    global acertos
-    print (perguntas[0]['Pergunta'])
-    opcao = (perguntas[0]['Opções'])
-    resultado = ('4', 'c', 'C')
+for pergunta in perguntas:
+    print ('Pergunta:', pergunta ['Pergunta'])
+    print ()
 
-    for i, opc in enumerate(opcao):
+    for i, opcao in enumerate(pergunta['Opções']):
         letra = string.ascii_uppercase[i]
-        print (f'{letra}){opc}')
+        print (f'{letra}) {opcao}')
+    print()
 
-    resposta = input('Escolha uma opção: ')
-    if resposta in resultado:
+    escolha = input ('Escolha uma opção: ')
+    if escolha in pergunta ['Resposta']:
         acertos += 1
         print ('Acertou!')
+        print()
 
     else:
         print ('Errou!')
+        print()
 
-    print (f'Você acertou {acertos} de 3 até agora!')
+if acertos >= 2:
+    print (f'Parabéns você acertou {acertos} perguntas!')
 
-# Função para a segunda pergunta
-def segunda_pergunta():
-    global acertos
-    print (perguntas[1]['Pergunta'])
-    opcao = (perguntas[1]['Opções'])
-    resultado = ('25', 'a', 'A')
+elif acertos == 1:
+    print (f'Você acertou apenas {acertos} pergunta.')
 
-    for i, opc in enumerate(opcao):
-        letra = string.ascii_uppercase[i]
-        print (f'{letra}){opc}')
-
-    resposta = input('Escolha uma opção: ')
-    if resposta in resultado:
-        acertos += 1
-        print ('Acertou!')
-
-    else:
-        print ('Errou!')
-
-    print (f'Você acertou {acertos} de 3 até agora!')
-
-# Função para a terceira pergunta
-def terceira_pergunta():
-    global acertos
-    print (perguntas[2]['Pergunta'])
-    opcao = (perguntas[2]['Opções'])
-    resultado = ('5', 'b', 'B')
-
-    for i, opc in enumerate(opcao):
-        letra = string.ascii_uppercase[i]
-        print (f'{letra}){opc}')
-
-    resposta = input('Escolha uma opção: ')
-    if resposta in resultado:
-        acertos += 1
-        print ('Acertou!')
-
-    else:
-        print ('Errou!')
-
-    print (f'Você acertou {acertos} de 3 até agora!')
-
-# Função para a mensagem final
-def mensagem_final():
-    if acertos == 3:
-        print ('Parabéns você acertou todas as perguntas!')
-
-    elif acertos == 2:
-        print ('Que bom! Você acertou duas perguntas, continue a estudar!')
-
-    elif acertos == 2:
-        print ('Infelizmente você não acertou nenhuma resposta, mas não desista e estude!')
-
-primeira_pergunta()
-segunda_pergunta()
-terceira_pergunta()
-mensagem_final()
+else:
+    print ('Você não acertou nenhuma pergunta.')
